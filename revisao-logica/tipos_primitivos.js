4612ee04259de81cd8fb9b400f7cc7cb72b5c0b9
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-04-26 09:22:47 -0300

    .

--- revisao-logica/tipos_primitivos.js
+++ revisao-logica/tipos_primitivos.js
@@ -1,3 +1,6 @@
+// Tipos primitivos: string, number, booler
+let nome = 'Kauan Gomes Marques'
 let idade = 17
-let texto = 'Eu tenho'
-console.log(texto)
+let feliz = true
+
+console.log('Ola meu nome e ${nome} tenho ${idade} e estou feliz?  resposta ${feliz}')
