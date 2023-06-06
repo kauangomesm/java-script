8f89032272695606258ad620fc2152f473f7e13f
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-06-06 14:31:04 -0300

    Create hoisting.js

---
+++ funções/hoisting.js
@@ -0,0 +1,15 @@
+funcao1()
+funcao()
+
+
+
+
+const funcao = function funcao(){ // a função so e executada quando o codico chegar nessa linha
+    console.log('Funcionou a funcao')
+}
+
+
+function funcao1(){
+    console.log('Funcionou a funcao1') // a função e executada no começo do codigo
+}
+
