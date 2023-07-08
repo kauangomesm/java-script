8059802438441e2f30db170d5f4f286bfc3b245e
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-07-08 12:29:58 -0300

    Update array05.js

--- Arrays/array05.js
+++ Arrays/array05.js
@@ -1,3 +1,18 @@
-// Metodos -- push(), pup(), shift(), unshit, slice(), splice()
-let arr
+// Metodos -- push(), pop(), shift(), unshit, slice(), splice()
+let arr = [1, 5, 3, 10, 'alo', 'que isso']
+console.log(arr)
 
+// arr.push(false, true, 'meu filho')
+
+// arr.pop()
+
+// arr.shift()
+
+// arr.unshift(1)
+
+// arr.splice()
+
+// arr.slice(-1)
+
+console.log(arr[arr.length-1])
+console.log(arr.slice(-1).toString())
