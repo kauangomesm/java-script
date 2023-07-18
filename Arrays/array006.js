cb38cacbe5f81a526d34317ae42fa90549f418b2
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-07-18 13:20:10 -0300

    Create array006.js

---
+++ Arrays/array006.js
@@ -0,0 +1,21 @@
+/*  every() -- some() -- forEach() -- filter()
+
+    map() -- indexOf() -- lastIndexOf() -- includes()
+
+    find() -- findIndex() -- toString() -- join() -- concat()
+
+    push() -- pop() -- shift() -- unshift() -- slice()
+
+    splice() -- reverse() -- reduce()
+*/
+
+let aray1 = [1, 2, 3, 4, 5]
+let aray2 = ['a', 'b', 'c', 'd', 'e']
+
+
+
+
+
+
+
+
