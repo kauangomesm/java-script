543a7bc86c8bd89a4b964a4405df1017e1d9a276
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-09-15 17:02:30 -0300

    .

--- formulario_original/js/formulario.js
+++ formulario_original/js/formulario.js
@@ -1,24 +1,66 @@
 (function () {
     'use strict';
-
-
     const titulo = document.querySelector('#txtTitulo')
     const descricao = document.querySelector('#txtDescricao')
-    const checkbox = document.querySelector('#checkbox')
+    const checkbox = document.querySelector('#chkAceito')
     const botao = document.querySelector('#btn')
     const form = document.querySelector('.formCadastro')
     const contador_container = document.querySelector('#contador')
     const contador = contador_container.getElementsByTagName('span')[0]
-    const maximo = descricao.maxLenght
+    const maximo = descricao.maxLength
+    const feedbakMessage = document.getElementById('feedbackMessage')
+    const PFeedbackMessage = document.querySelector('#feedbackMessage p')
+    const closeFeedbackMessage = document.querySelector('#feedbackMessage button')
+
     form.addEventListener('submit', (evt) => {
         if(!titulo.value){
-            alert('campo vazio')
+            showErrorMsg('campo vazio', () => {
+                titulo.focus()
+            })
             evt.preventDefault()
-            titulo.focus()
         }
     })
 
+    function showErrorMsg(msg, cb){
+        feedbakMessage.classList.add('show')
+        PFeedbackMessage.textContent = msg
+
+        closeFeedbackMessage.focus()
+
+        function hideFeedbackmessage(){
+            feedbakMessage.classList.remove('show')
+            closeFeedbackMessage.removeEventListener('click', hideFeedbackmessage)
+            closeFeedbackMessage.removeEventListener('keyup', presskeybord)
+            
+            if (typeof(cb) === 'function'){
+                cb()
+            }
+        }
+
+        function presskeybord(evt){
+            if(evt.keyCode === 27){
+                hideFeedbackmessage()
+            }
+        }
+
+
+        closeFeedbackMessage.addEventListener('click', hideFeedbackmessage)
+        closeFeedbackMessage.addEventListener('keyup', presskeybord)
+
+    }
 
     contador_container.style.display = 'block'
+    contador.textContent = parseInt(maximo)
+
+
+    descricao.addEventListener("input", function () {
+        contador.textContent = parseInt(maximo) - parseInt(descricao.value.length)
+    })
+
+
+    botao.disabled = true
+    checkbox.addEventListener('change', function () {
+        botao.disabled = !this.checked
+    })
 
 })()
