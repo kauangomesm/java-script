b5c29141ae24c6a71313007ef49ed3b3f33b2ac5
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-09-26 11:23:20 -0300

    .

--- lista de tarefas/js/todolist.js
+++ lista de tarefas/js/todolist.js
@@ -1,4 +1,4 @@
-(function (){
+;(function (){
 
     const botaoAdicionar = document.querySelector('#todo-add')
     const nomeAdicionar = document.querySelector('#item-input')
@@ -12,20 +12,60 @@
         }
     ]
 
-    function addEventLi(li){
-        li.addEventListener('click', function () {
-            console.log(this)
-        })
-    }
+  
 
     function createli(obj){
         const li = document.createElement('li')
-        li.classList.add('todo-item')
         const p = document.createElement('p')
+        const checkbutton = document.createElement('button')
+        const editbutton = document.createElement('i')
+        const deletebutton = document.createElement('i')
+        const icheck = document.createElement('i')
+        const editContainer = document.createElement('div')
+        const editInput = document.createElement("input")
+        const d_editbutton = document.createElement('button')
+        const d_cancelbutton = document.createElement('button')
+
+
+        li.classList.add('todo-item')
+
+        checkbutton.className = "button-check"
+        checkbutton.setAttribute("data-action", 'checkbutton')
+        icheck.className = "fas fa-check displayNone"
+        checkbutton.appendChild(icheck)
+        li.appendChild(checkbutton)
+
         p.classList.add('task-name')
         p.textContent = obj.name
         li.appendChild(p)
-        addEventLi(li)
+
+        editbutton.className = "fas fa-edit"
+        editbutton.setAttribute("data-action", "editbutton")
+        li.appendChild(editbutton)
+
+        editContainer.className = "editContainer"
+
+        editInput.className = "editInput"
+        editInput.setAttribute("type", "text")
+        editContainer.appendChild(editInput)
+
+        d_editbutton.className = "editButton"
+        d_editbutton.setAttribute("data-action", "d_editbutton")
+        d_editbutton.textContent = "Edit"
+        editContainer.appendChild(d_editbutton)
+
+        d_cancelbutton.className = "cancelButton"
+        d_cancelbutton.setAttribute("data-action", "d_cancelbutton")
+        d_cancelbutton.textContent = "Cancel"
+        editContainer.appendChild(d_cancelbutton)
+
+        li.appendChild(editContainer)
+
+        deletebutton.className = "fas fa-trash-alt"
+        deletebutton.setAttribute('data-action', 'deletebutton')
+        li.appendChild(deletebutton)
+
+
         return li
     }
 
